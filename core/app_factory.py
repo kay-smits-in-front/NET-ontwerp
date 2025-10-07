e938458971de05856b4e45e08bbf653fa248a8f4
KaySmitsInfront <k.smits@in-front.nl>
2025-10-07 10:25:54 +0200

    # add correct files for heroku deployement

--- core/app_factory.py
+++ core/app_factory.py
@@ -4,7 +4,7 @@ import os
 
 
 def create_app():
-	# Add project root to Python path
+	# Add project root to python path
 	project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 	if project_root not in sys.path:
 		sys.path.insert(0, project_root)
