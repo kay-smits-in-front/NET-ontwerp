1c16d684d8039da582889ba96db0ddfac766a060
KaySmitsInfront <k.smits@in-front.nl>
2025-10-07 13:46:58 +0200

    # removed test app

--- apps/test_app1/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint('test_app1', __name__, url_prefix='/test_app1')
-
-@bp.route('/')
-def main():
-    return render_template('test_app1/test_app1.html',
-                         title='Test App 1',
-                         data={'message': 'Dit is de eerste test applicatie!'})
